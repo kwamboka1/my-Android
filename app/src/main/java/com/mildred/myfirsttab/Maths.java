7f947b3e5336bc87337bdd54b44581c3fd8558c0
STUDENT <STUDENT>
2016-02-13 16:31:26 +0300

    first commit

---
+++ app/src/main/java/com/mildred/myfirsttab/Maths.java
@@ -0,0 +1,32 @@
+package com.mildred.myfirsttab;
+
+/**
+ * Created by STUDENT on 2/6/2016.
+ */
+public class Maths{
+
+
+   public static void main(String args[]){
+       //call a method by the method name
+       addition();
+       subtraction();
+   }
+    //Addition method
+
+   public static void addition(){
+       int x = 50;
+       int y = 5;
+        int answer = x+y;
+       System.out.println(answer);
+
+   }
+    //Subtraction method
+
+    public static void subtraction(){
+        int a = 20;
+        int b = 5;
+        int answer = a-b;
+        System.out.println(answer);
+
+    }
+}
---
+++ app/src/main/java/com/mildred/myfirsttab/TestClass.java
@@ -0,0 +1,32 @@
+package com.mildred.myfirsttab;
+
+/**
+ * Created by STUDENT on 2/6/2016.
+ */
+public class TestClass {
+
+
+    public static void main(String args []){
+        //Declare a variable
+        int age;
+        double shoeSize;
+        char letter;
+        String firstName;
+
+        //Initialise the variable
+        age=10;
+        shoeSize = 5.5;
+        letter ='A';
+        firstName = "Mildred";
+        System.out.println(age);
+        System.out.println(shoeSize);
+        System.out.println(letter);
+    }
+    //Addition method.
+public void addition(){
+
+}
+    public void subtraction(){
+
+    }
+}
